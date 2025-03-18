49678ab4ea0915286bca7d7163b706914d96428f
LiudasBaronas1 <144480589+LiudasBaronas1@users.noreply.github.com>
2025-03-18 17:25:18 +0200

    3rd commit

--- EmployeeApplication/src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
+++ EmployeeApplication/src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.employee.EmployeeApplication.entity.Employee;
 import com.employee.EmployeeApplication.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,15 @@ public class EmployeeController {
     public List<Employee> findAllEmployees(){
 
         return employeeService.getAllEmployees();
+    }
+
+    @RequestMapping("/employees/{id}")
+    public Employee findAnEmployee(@PathVariable int id){
+        return employeeService.getAnEmployee(id);
+    }
+
+    @RequestMapping("/employees")
+    public void createEmployee(Employee employee){
+        employeeService.createEmployee(employee);
     }
 }
--- EmployeeApplication/src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
+++ EmployeeApplication/src/main/java/com/employee/EmployeeApplication/service/EmployeeService.java
@@ -3,17 +3,27 @@ package com.employee.EmployeeApplication.service;
 import com.employee.EmployeeApplication.entity.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    List<Employee> employeeList = Arrays.asList(
+    List<Employee> employeeList = new ArrayList<>(Arrays.asList(
             new Employee(1,"First Employee", "Washington"),
             new Employee(2,"Second Employee", "New York")
-    );
+    ));
 
     public List<Employee> getAllEmployees() {
         return employeeList;
+    }
+
+    public Employee getAnEmployee(int id){
+        return employeeList.stream().filter(e -> (
+                e.getEmployeeId() == id)).findFirst().get();
+    }
+
+    public void createEmployee(Employee employee){
+        employeeList.add(employee);
     }
 }
