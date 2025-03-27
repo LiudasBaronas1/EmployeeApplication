f53605cb5f46dbd7b00422562b8513ef5eb6b15b
LiudasBaronas1 <144480589+LiudasBaronas1@users.noreply.github.com>
2025-03-27 15:56:41 +0200

    5th commit, fixed h2 database

--- EmployeeApplication/src/main/java/com/employee/EmployeeApplication/EmployeeApplication.java
+++ EmployeeApplication/src/main/java/com/employee/EmployeeApplication/EmployeeApplication.java
@@ -2,6 +2,7 @@ package com.employee.EmployeeApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class EmployeeApplication {
